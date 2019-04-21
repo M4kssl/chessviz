eca27348c55f2f4ae43842940d5f9fedcce5c770
Max <Maks.latsigin@mail.ru>
2019-04-21 21:54:27 +0700

    Some fixes in main.c

--- src/main.c
+++ src/main.c
@@ -3,25 +3,32 @@
 #include <stdlib.h>
 #include <string.h>
 
-int main()
+int main(int argc, char* argv[])
 {
-    {
-        char Board[8][8] = {{'r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'},
-                            {'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'},
-                            {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-                            {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-                            {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-                            {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-                            {'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'},
-                            {'R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'}};
+    char Board[8][8] = {{'r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'},
+                        {'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'},
+                        {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+                        {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+                        {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+                        {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+                        {'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'},
+                        {'R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'}};
 
-        FILE* fp;
-        fp = fopen(argv[1], "a+");
-        while (!feof(input)) {
-            char sw[10], sb[10], str[2];
-            fscanf(input, "%s %s %s", str, sw, sb);
+    FILE* fp;
+    switch (argc) {
+        FILE* input;
+        switch (argc) {
+        case 2:
+            input = fopen(argv[1], "a+");
+            while (!feof(input)) {
+                char sw[10], sb[10], str[2];
+                fscanf(input, "%s %s %s", str, sw, sb);
+            }
+            break;
+        default:
+            printf("ERROR");
+            return 1;
         }
-        break;
 
         fclose(input);
 
